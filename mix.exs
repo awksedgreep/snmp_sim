35585d0c6081bf406023256bbdac27c9ab303245
Mark Cotner <mark.cotner@gmail.com>
2025-05-31 09:38:02 -0400

    Fix bug in device stop

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SNMPSimEx.MixProject do
   def project do
     [
       app: :snmp_sim_ex,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
@@ -34,7 +34,7 @@ defmodule SNMPSimEx.MixProject do
   defp releases do
     [
       snmp_sim_ex: [
-        version: "0.1.0",
+        version: "0.1.1",
         applications: [snmp_sim_ex: :permanent],
         steps: [:assemble, :tar],
         strip_beams: Mix.env() == :prod,
